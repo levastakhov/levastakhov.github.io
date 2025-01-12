7b854fa72cf5757bae444ac61d22bdcf122b9527
Lev Astakhov <Lev Astakhov>
2025-01-12 13:41:23 +0500

    new

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,21 +1,53 @@
-// Firebase Messaging Service Worker
+const staticCacheName = 's-app-v3'
+const dynamicCacheName = 'd-app-v3'
 
-self.addEventListener("push", (event) => {
+const assetUrls = [
+  'index.html',
+  'js/app.js',
+  'css/styles.css',
+  'offline.html'
+]
 
-    const notif = event.data.json().notification;
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)
+})
 
-    event.waitUntil(self.registration.showNotification(notif.title , {
-        body: notif.body,
-        icon: notif.image,
-        data: {
-            url: notif.click_action
-        }
-    }));
+self.addEventListener('activate', async event => {
+  const cacheNames = await caches.keys()
+  await Promise.all(
+    cacheNames
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+      .map(name => caches.delete(name))
+  )
+})
 
-});
+self.addEventListener('fetch', event => {
+  const {request} = event
 
-self.addEventListener("notificationclick", (event) => {
+  const url = new URL(request.url)
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request))
+  } else {
+    event.respondWith(networkFirst(request))
+  }
+})
 
-    event.waitUntil(clients.openWindow(event.notification.data.url));
 
-});
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (e) {
+    const cached = await cache.match(request)
+    return cached ?? await caches.match('/offline.html')
+  }
+}
